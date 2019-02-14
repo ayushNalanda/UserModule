8ccb302273be7363366798c3959325a094601d11
AYUSH <AYUSH@DESKTOP-NHBAJIS>
2019-02-14 19:30:28 +0530

    Commit

---
+++ UserModule-2/src/main/java/com/ltd/pvt/demo/user/api/UserModule2Application.java
@@ -0,0 +1,14 @@
+package com.ltd.pvt.demo.user.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserModule2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserModule2Application.class, args);
+	}
+
+}
+
