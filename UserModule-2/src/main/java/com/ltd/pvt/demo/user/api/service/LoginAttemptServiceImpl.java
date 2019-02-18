3ba54dbb76aabaf705c7dc0a0207655c6cc2336d
AYUSH <aaush0101@gmail.com>
2019-02-18 23:29:19 +0530

    commit

--- UserModule-2/src/main/java/com/ltd/pvt/demo/user/api/service/LoginAttemptServiceImpl.java
+++ UserModule-2/src/main/java/com/ltd/pvt/demo/user/api/service/LoginAttemptServiceImpl.java
@@ -3,6 +3,9 @@ package com.ltd.pvt.demo.user.api.service;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.ExecutionException;
@@ -12,11 +15,15 @@ import java.util.concurrent.TimeUnit;
 @Service
 public class LoginAttemptServiceImpl implements  LoginAttemptService {
 
+	
+	private static final Logger log = LoggerFactory.getLogger(LoginAttemptServiceImpl.class);
+
     private final int MAX_ATTEMPT = 3;
     private LoadingCache<String, Integer> attemptsCache;
 
     public LoginAttemptServiceImpl() {
         super();
+       log.debug("LoginAttemptServiceImpl.LoginAttemptServiceImpl()");
         attemptsCache = CacheBuilder.newBuilder().expireAfterWrite(2, TimeUnit.MINUTES).build(new CacheLoader<String, Integer>() {
             @Override
             public Integer load(final String key) {
@@ -26,11 +33,13 @@ public class LoginAttemptServiceImpl implements  LoginAttemptService {
     }
 
     public void loginSucceeded(String key) {
+    	log.debug("LoginAttemptServiceImpl.loginSucceeded()");
         attemptsCache.invalidate(key);
     }
 
     public void loginFailed(String key) {
         int attempts = 0;
+        log.debug("LoginAttemptServiceImpl.loginFailed()");
         try {
             attempts = attemptsCache.get(key);
         } catch (ExecutionException e) {
@@ -41,10 +50,12 @@ public class LoginAttemptServiceImpl implements  LoginAttemptService {
     }
 
     public boolean isBlocked(String key) {
+    	log.debug("LoginAttemptServiceImpl.isBlocked()");
         try {
+        	
             return attemptsCache.get(key) >= MAX_ATTEMPT;
         } catch (ExecutionException e) {
-            System.out.println(e.toString());
+            log.warn("You are blocked     "+e.getMessage());
             return false;
         }
     }
